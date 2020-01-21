73ba41aa5704afe86621370ec1bc59164c159d4a
Thomas Kindler <virtualdreamsdd@aol.com>
2020-01-21 14:52:12 +0100

    Added new API endpoints.
    Added API endpoint to get all categories and tags.
    Updated postman collection.

---
+++ ApiControllers/CategoryController.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ltbdb.Core.Services;
+
+namespace ltbdb.WebAPI.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/v1/[controller]")]
+	[Authorize(Policy = "AdministratorOnly", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public class CategoryController : Controller
+	{
+		private readonly IMapper Mapper;
+		private readonly Settings Options;
+		private readonly BookService BookService;
+		private readonly CategoryService CategoryService;
+		private readonly TagService TagService;
+
+		public CategoryController(IMapper mapper, IOptionsSnapshot<Settings> settings, BookService book, CategoryService category, TagService tag)
+		{
+			Mapper = mapper;
+			Options = settings.Value;
+			BookService = book;
+			CategoryService = category;
+			TagService = tag;
+		}
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var _categories = CategoryService.Get();
+			return Ok(_categories);
+		}
+	}
+}
---
+++ ApiControllers/TagController.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ltbdb.Core.Services;
+
+namespace ltbdb.WebAPI.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/v1/[controller]")]
+	[Authorize(Policy = "AdministratorOnly", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public class TagController : Controller
+	{
+		private readonly IMapper Mapper;
+		private readonly Settings Options;
+		private readonly BookService BookService;
+		private readonly CategoryService CategoryService;
+		private readonly TagService TagService;
+
+		public TagController(IMapper mapper, IOptionsSnapshot<Settings> settings, BookService book, CategoryService category, TagService tag)
+		{
+			Mapper = mapper;
+			Options = settings.Value;
+			BookService = book;
+			CategoryService = category;
+			TagService = tag;
+		}
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var _tags = TagService.Get();
+			return Ok(_tags);
+		}
+	}
+}

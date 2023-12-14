3706b0c8d31473e06bd621e4687b328a279bad95
Thomas Kindler <virtualdreamsdd@aol.com>
2023-12-14 13:33:52 +0100

    Added database migrations based on FluentMigrator.

---
+++ src/LtbDb2/Migrations/Schema/Postgres/migration_1.sql
@@ -0,0 +1,32 @@
+-- book table
+create table book (
+  id serial primary key,
+  number int not null, 
+  title varchar(200) not null,
+  category varchar(100) not null,
+  created timestamptz not null,
+  modified timestamptz not null,
+  filename varchar(100) default null
+);
+
+-- story table
+create table story (
+  id serial primary key,
+  name varchar(200) not null,
+  item_order int not null default 0,
+  bookid int not null
+);
+
+-- tag table
+create table tag (
+  id serial primary key,
+  name varchar(50) not null,
+  bookid int not null
+);
+
+-- create indexes
+create index ix_book_category on book(category);
+create index ix_story_name on story(name);
+create index ix_story_bookid on story(bookid);
+create index ix_tag_name on tag(name);
+create index ix_tag_bookid on tag(bookid);
---
+++ src/LtbDb2/Migrations/VersionInfo.cs
@@ -0,0 +1,24 @@
+using FluentMigrator.Runner.VersionTableInfo;
+
+namespace LtbDb.Migrations
+{
+	[VersionTableMetaData]
+	public class VersionInfo : IVersionTableMetaData
+	{
+		public object ApplicationContext { get; set; }
+
+		public bool OwnsSchema => true;
+
+		public string SchemaName => "public";
+
+		public string TableName => "schema";
+
+		public string ColumnName => "version";
+
+		public string DescriptionColumnName => "description";
+
+		public string UniqueIndexName => "ix_schema_version";
+
+		public string AppliedOnColumnName => "applied_on";
+	}
+}

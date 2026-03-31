0e7ba1bb554c06b31a2e6deda809df0be5cb65cc
Thomas Kindler <virtualdreamsdd@aol.com>
2026-03-31 12:07:13 +0200

    Removed unused code.

--- src/LtbDb2/WebAPI/V1/FluentValidation/FluentValidationExtensions.cs
+++ src/LtbDb2/WebAPI/V1/FluentValidation/FluentValidationExtensions.cs
@@ -22,24 +22,6 @@ namespace LtbDb.WebAPI.V1.FluentValidation
 				}
 
 				return _response;
-
-				// var _errorsInModelState = context.ModelState
-				// 	.Where(w => w.Value.Errors.Count > 0)
-				// 	.ToDictionary(d => d.Key, d => d.Value.Errors.Select(s => s.ErrorMessage)).ToArray();
-
-				// var _response = new List<ErrorResponse>();
-
-				// foreach (var error in _errorsInModelState)
-				// {
-				// 	_response.Add(new ErrorResponse
-				// 	{
-				// 		Field = error.Key,
-				// 		Messages = error.Value.ToList()
-				// 	});
-				// }
-
-				// context.Result = new BadRequestObjectResult(_response);
-				// return true;
 			}
 
 			return null;
